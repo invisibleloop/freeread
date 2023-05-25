906163ebfdd7555dc233824e03b9d81c0d39276b
Andy Stubbs <ink@invisibleloop.com>
2023-05-25 10:01:56 +0100

    feat: first commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# ReadBook
+
+ReadBook is a Node.js script that allows you to download and read books from Project Gutenberg right in your terminal. It offers a unique and distraction-free reading experience.
+
+## Prerequisites
+
+- [Node.js](https://nodejs.org/en/download/) and npm (comes bundled with Node.js)
+- [Calibre](https://calibre-ebook.com/download) for the `ebook-convert` command-line tool
+
+## Installation
+
+1. **Clone or download this repository** to your local machine.
+
+2. **Navigate to the root directory of the script** in your terminal (the directory where the `package.json` file is located).
+
+3. **Install the script's dependencies** by running:
+
+    ```shell
+    npm install
+    ```
+
+4. **Ensure `ebook-convert` is installed** and available in your terminal by running:
+
+    ```shell
+    ebook-convert --version
+    ```
+
+    This should print the version of `ebook-convert` if it's installed correctly.
+
+    If not installed you can install using Homebrew (macOS).
+
+    ```shell
+    brew install calibre
+    ```
+
+## Usage
+
+Once everything is set up, you can run the script using the npm script commands defined in the `package.json` file:
+
+- **To read in light mode**, use:
+
+    ```shell
+    npm run read
+    ```
+
+- **To read in dark mode**, use:
+
+    ```shell
+    npm run read:dark
+    ```
+
+These commands will present you with a list of books. Choose one to download and read.
+
+In this first version of "readbook" there are only two books to choose from. Eventually there will the ability to search for books available for free on Project Gutenberg.
+
+While reading, you can scroll through the book using the arrow keys or the space bar. To exit the book and the script, press `q`. Upon exiting, the script will delete the downloaded and generated files.
+
+## Troubleshooting
+
+If you encounter issues:
+
+- Make sure you have the latest version of Node.js and npm installed. The script may not work properly with older versions.
+- Verify that you've installed all dependencies with `npm install`.
+- Check if `ebook-convert` is properly installed and available in your terminal.
+- If you still encounter problems, feel free to open an issue in this repository.
+
+## License
+
+This project is licensed under the ISC License.
---
+++ read.js
@@ -0,0 +1,113 @@
+import axios from "axios";
+import inquirer from "inquirer";
+import fs from "fs";
+import wrap from "wrap-ansi";
+import { spawn } from "child_process";
+import ora from "ora";
+import chalk from "chalk";
+import yargs from "yargs";
+
+const books = [
+  {
+    title: "Alice's Adventures in Wonderland by Lewis Carroll",
+    downloadLink: "https://www.gutenberg.org/ebooks/11.epub.noimages",
+  },
+  {
+    title: "The Time Machine by H. G. Wells",
+    downloadLink: "https://www.gutenberg.org/ebooks/35.epub.noimages",
+  },
+];
+
+async function downloadEpub(book) {
+  const response = await axios.get(book.downloadLink, {
+    responseType: "stream",
+  });
+  const writer = fs.createWriteStream(`${book.title}.epub`);
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+}
+
+async function convertEpubToTxt(bookTitle) {
+  return new Promise((resolve, reject) => {
+    const converter = spawn("ebook-convert", [
+      `${bookTitle}.epub`,
+      `${bookTitle}.txt`,
+    ]);
+
+    converter.on("close", resolve);
+    converter.on("error", reject);
+  });
+}
+
+async function formatTextWidth(bookTitle, mode) {
+  const text = fs.readFileSync(`${bookTitle}.txt`, "utf8");
+  const formattedText = wrap(text, 70);
+  fs.writeFileSync(
+    `${bookTitle}_formatted.txt`,
+    mode === "light" ? chalk.black(formattedText) : chalk.white(formattedText)
+  );
+}
+
+async function selectAndDownloadBook() {
+  const argv = yargs(process.argv.slice(2)).option("mode", {
+    alias: "m",
+    describe: "Select the mode: light or dark",
+    choices: ["light", "dark"],
+    default: "light",
+  }).argv;
+
+  const mode = argv.mode;
+
+  const { book } = await inquirer.prompt([
+    {
+      type: "list",
+      name: "book",
+      message: "Select a book to download and read:",
+      choices: books.map((book) => book.title),
+    },
+  ]);
+
+  const selectedBook = books.find((b) => b.title === book);
+
+  if (selectedBook) {
+    const spinner = ora("Downloading and formatting text.").start();
+    await downloadEpub(selectedBook);
+    await convertEpubToTxt(selectedBook.title);
+    await formatTextWidth(selectedBook.title, mode);
+    spinner.stop();
+
+    const bgColor = mode === "light" ? "ff/ff/ff" : "0/0/0";
+
+    // Change the background color of the terminal window
+    process.stdout.write(`\x1b]11;rgb:${bgColor}\x1b\\`);
+
+    const lessProcess = spawn("less", [`${selectedBook.title}_formatted.txt`], {
+      stdio: "inherit",
+    });
+
+    // Add event listener for the exit event of the process
+    process.on("exit", () => {
+      // Reset the terminal color to default
+      process.stdout.write("\x1b]11;rgb:0/0/0\x1b\\");
+      // Remove the generated files
+      fs.unlinkSync(`${selectedBook.title}.epub`);
+      fs.unlinkSync(`${selectedBook.title}.txt`);
+      fs.unlinkSync(`${selectedBook.title}_formatted.txt`);
+    });
+
+    // Handle user quitting using Ctrl+C
+    process.on("SIGINT", () => {
+      lessProcess.kill(); // Kill the "less" process
+      process.exit(); // Exit the application
+    });
+  } else {
+    console.log("The selected book could not be found.");
+  }
+}
+
+selectAndDownloadBook();
